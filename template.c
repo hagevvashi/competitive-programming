647f6db6d1d7994fc31f2f2f0db275feb8177568
hagevvashi <hagevvashi@gmail.com>
2020-08-19 20:16:53 +0900

    chore(template): add macros(ll, INF, max and min)

--- template.c
+++ template.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 #define rep(i, n) for (int i = 0; i < n; i = i + 1)
-#define MAX 10010
-#define MAX_INT 9223372036854775807
 #define array(N, t) (t*)calloc(N, sizeof(t))
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
 
 int main() {
   return 0;
