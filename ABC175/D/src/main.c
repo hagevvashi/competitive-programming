03f009fd2a7ce680ffc0a32fb68fca4ea4f8c508
hagevvashi <hagevvashi@gmail.com>
2020-08-20 10:15:19 +0900

    wip: ABC175-D 模範解答を模写

--- ABC175/D/src/main.c
+++ ABC175/D/src/main.c
@@ -20,26 +20,61 @@ int main() {
   ll ans = -INF;
 
   // 開始位置を0から順番に試す
-  rep(i, N) {
-    // current_position: 移動した先
-    int current_position = i;
+  // max を求める
+  rep(si, N) {
+    // 常に現在地を示す
+    // 移動した先の今の場所を示すこともある
+    // まずは、今の位置である i を設定
+    int current_position = si;
 
     // 移動先のマスに書かれたscoreの配列を取っておく
     int score_array[10010] = {0};
     int score_index = 0;
 
-    // この中でPのに従って移動を行う
+    // 1周期の和が正か負か判定するために取っておく
+    ll total = 0;
+
+    // この中でPに従って移動を行う
     // 周期に入るのを検出したらそこで終わる
     while(1) {
+      // 現在地を更新
       current_position = P[current_position];
       score_array[score_index] = C[current_position];
-      if (i == current_position) {
+      total += C[current_position];
+      // 周期判定
+      // 移動した先が現在地だったら終わり
+      if (si == current_position) {
         break;
       }
       score_index += 1;
     }
+
+    // 周期の長さ
+    int period_length = score_index + 1;
+    // 今までの累積和
+    // あるところで切った時の累積
+    ll now = 0;
     // scoreの配列のどこで切るのかというのを試す
-    // TBD
+    // period_length 箇所の内どこで切る？
+    rep(i, period_length) {
+      now += score_array[i];
+      // Kが1の時に2の場所で区切ることはできないから
+      if (i + 1 > K) {
+        break;
+      }
+      // まず1周期の和で場合分けを行う
+      // 負の場合は前の方で切る方が良い
+      // 正の場合は後ろの方で切る方が良い
+      // 正の場合は可能な限り積む
+      if (total > 0) {
+        // 残り回数
+        ll rest = (K - (i + 1));
+        // 可能な回数
+        ll possibility = rest / period_length;
+        now += total * possibility;
+      }
+      ans = max(ans, now);
+    }
   }
 
   printf("%lld\n", ans);
