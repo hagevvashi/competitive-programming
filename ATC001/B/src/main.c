4d46465c5dee648b9924b9616a3417e059e1467c
hagevvashi <hagevvashi@gmail.com>
2020-09-03 10:08:07 +0900

    feat: finish ATC001-B union find implementation only by path compression

--- ATC001/B/src/main.c
+++ ATC001/B/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define rep(i, n) for (int i = 0; i < n; i = i + 1)
 #define array(N, t) (t*)calloc(N, sizeof(t))
@@ -8,9 +9,39 @@
 #define max(p, q)((p) > (q) ? (p) : (q))
 #define min(p, q)((p) < (q) ? (p) : (q))
 
+int * parent;
+
+int root(int x) {
+  if (parent[x] == x) {
+    return x;
+  }
+  return parent[x] = root(parent[x]);
+}
+
+int same(int x, int y) {
+  return root(x) == root(y);
+}
+
+void unite(int x, int y) {
+  x = root(x);
+  y = root(y);
+
+  if (x == y) {
+    return;
+  }
+
+  parent[x] = y;
+}
+
 int main() {
   int N, Q;
   scanf("%d%d", &N, &Q);
+
+  // initialize
+  parent = array(N, int);
+  rep(i, N) {
+    parent[i] = i;
+  }
 
   int * P = array(Q, int);
   int * A = array(Q, int);
@@ -19,6 +50,15 @@ int main() {
     scanf("%d%d%d", P + i, A + i, B + i);
   }
 
+  rep(i, Q) {
+    if (P[i] == 0) {
+      unite(A[i], B[i]);
+    } else {
+      printf("%s\n", (same(A[i], B[i]) ? "Yes" : "No"));
+    }
+  }
+
+  free(parent);
   free(P);
   free(A);
   free(B);
