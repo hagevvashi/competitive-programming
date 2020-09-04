9590a52746e0368ef45b472afbc2f98fabd696bf
hagevvashi <hagevvashi@gmail.com>
2020-09-04 10:14:10 +0900

    refactor: add ATC001-B debugging comment

--- ATC001/B/src/main.c
+++ ATC001/B/src/main.c
@@ -9,12 +9,18 @@
 #define max(p, q)((p) > (q) ? (p) : (q))
 #define min(p, q)((p) < (q) ? (p) : (q))
 
+int N;
 int * parent;
 
 int root(int x) {
+  // printf("x: %d\n", x);
   if (parent[x] == x) {
+    // printf("//// parent[x] == x ////\n");
     return x;
   }
+
+  // printf("//// parent[x] != x ////\n");
+  // printf("//// parent[x] = root(parent[x]); ////\n");
   return parent[x] = root(parent[x]);
 }
 
@@ -23,18 +29,62 @@ int same(int x, int y) {
 }
 
 void unite(int x, int y) {
+  // printf("unite(%d, %d)\n", x, y);
+
+  // rep(i, N) {
+  //   printf("parent[%d]: ", i);
+  //   printf("%d\n", parent[i]);
+  // }
+
+  // printf("root(%d): \n", x);
   x = root(x);
+  // printf("x...%d\n", x);
+
+  // rep(i, N) {
+  //   printf("parent[%d]: ", i);
+  //   printf("%d\n", parent[i]);
+  // }
+
+  // printf("root(%d): \n", y);
   y = root(y);
+  // printf("y...%d\n", y);
+
+  // rep(i, N) {
+  //   printf("parent[%d]: ", i);
+  //   printf("%d\n", parent[i]);
+  // }
 
   if (x == y) {
+    // printf("//// x == y ////\n");
+    // printf("\n");
     return;
   }
 
+  // printf("//// x != y ////\n");
+  // printf("//// parent[x] = y; ////\n");
   parent[x] = y;
+  // rep(i, N) {
+  //   printf("parent[%d]: ", i);
+  //   printf("%d\n", parent[i]);
+  // }
+  // printf("\n");
 }
 
+/**
+ *
+ * e.g.
+ *
+ * 5 4
+ * 0 0 1
+ * 0 2 3
+ * 0 0 2
+ * 1 0 3
+ *
+ * Yes
+ *
+ */
 int main() {
-  int N, Q;
+  int Q;
   scanf("%d%d", &N, &Q);
 
   // initialize
