ffa2918b60c562cfa9e2181c21d5d5ebee5be644
hagevvashi <hagevvashi@gmail.com>
2020-09-10 09:43:30 +0900

    feat: add size function to union_find lib

--- lib/union_find/src/main.c
+++ lib/union_find/src/main.c
@@ -8,49 +8,51 @@
 #define INF ((1LL << 62) - (1LL << 31))
 #define max(p, q)((p) > (q) ? (p) : (q))
 #define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(type, a, b) { type temp = a; a = b; b = temp; }
 
 int N;
-int * parent;
-int * rank;
+int * data;
 
-int root(int x) {
-  if (parent[x] == x) {
+void init() {
+  // child: parent's index
+  // root: -size
+  data = array(N, int);
+
+  rep(i, N) {
+    // at first, all data is root and size is -1
+    data[i] = -1;
+  }
+}
+
+int find(int x) {
+  if (data[x] < 0) {
     return x;
   }
 
-  return parent[x] = root(parent[x]);
+  return data[x] = find(data[x]);
 }
 
 int same(int x, int y) {
-  return root(x) == root(y);
+  return find(x) == find(y);
 }
 
-void unite(int x, int y) {
-  x = root(x);
-  y = root(y);
-
+int unite(int x, int y) {
+  x = find(x);
+  y = find(y);
   if (x == y) {
-    return;
+    return 0;
   }
-
-  if (rank[x] < rank[y]) {
-    parent[x] = y;
-  } else {
-    parent[y] = x;
-    if (rank[x] == rank[y]) {
-      rank[x] += 1;
-    }
+  // becasue data has minus size, reverse the inequality sign
+  if (data[x] > data[y]) {
+    swap(int, x, y);
   }
+  data[x] += data[y];
+  data[y] = x;
+  return 1;
 }
 
-void init() {
-  parent = array(N, int);
-  rank = array(N, int);
-
-  rep(i, N) {
-    parent[i] = i;
-    rank[i] = 0;
-  }
+int size(int x) {
+  return -data[find(x)];
 }
 
 int main() {
@@ -61,18 +63,24 @@ int main() {
 
   int i= N - N;
 
+  printf("size of %d is %d\n", i, size(i));
+  printf("unite %d and %d\n", i, i + 1);
   unite(i, i + 1);
+  printf("size of %d is %d\n", i, size(i));
+  printf("unite %d and %d\n", i + 2, i + 3);
   unite(i + 2, i + 3);
+  printf("size of %d is %d\n", i, size(i));
+  printf("unite %d and %d\n", i, i + 2);
   unite(i, i + 2);
+  printf("size of %d is %d\n", i, size(i));
 
   if (same(i, i + 3)) {
-    printf("same\n");
+    printf("%d and %d are the same\n", i, i + 3);
   } else {
-    printf("not same\n");
+    printf("%d and %d are not the same\n", i, i + 3);
   }
 
-  free(parent);
-  free(rank);
+  free(data);
 
   return 0;
 }
