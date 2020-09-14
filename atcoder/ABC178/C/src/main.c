013b1fe37512e4a6431c9d8a9ad15342f0fb925d
hagevvashi <hagevvashi@gmail.com>
2020-09-14 09:56:08 +0900

    feat: add ABC178-C correct answer

--- atcoder/ABC178/C/src/main.c
+++ atcoder/ABC178/C/src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define rep(type, i, n) for (type i = 0; i < n; i = i + 1)
 #define array(N, t) (t*)calloc(N, sizeof(t))
 #define ll long long
 #define INF ((1LL << 62) - (1LL << 31))
@@ -9,8 +9,38 @@
 #define min(p, q)((p) < (q) ? (p) : (q))
 #define swap(type, a, b) { type temp = a; a = b; b = temp; }
 
+const ll mod = 1000000007;
+
+ll powmod(ll x, ll y) {
+  ll res = 1;
+  rep(ll, i, y) {
+    res *= x;
+    res %= mod;
+  }
+  return res;
+}
+
 int main() {
-  
+  ll N;
+  scanf("%lld", &N);
+
+  ll a = powmod(10, N);
+  // printf("a: %lld\n", a);
+
+  ll b = powmod(9, N);
+  // printf("b: %lld\n", b);
+
+  ll c = b;
+  // printf("c: %lld\n", c);
+
+  ll d = powmod(8, N);
+  // printf("d: %lld\n", d);
+
+  ll ans = (a + d) - (b + c);
+  ans %= mod;
+  ans = (ans + mod) % mod;
+
+  printf("%lld\n", ans);
 
   return 0;
 }
