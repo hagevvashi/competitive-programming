1bbb9bfa09cfaed5d4d432cebd23928b7310df3b
hagevvashi <hagevvashi@gmail.com>
2020-09-29 09:19:41 +0900

    feat: add ABC156-A AC ans

---
+++ atcoder/ABC156/A/src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(type, a, b) { type temp = a; a = b; b = temp; }
+
+int main() {
+  int N, R;
+  scanf("%d%d", &N, &R);
+
+  int diff = N < 10 ? 100 * (10 - N) : 0;
+
+  printf("%d\n", R + diff);
+
+  return 0;
+}
