8d12b737f09386a9f8faf21807343c1b659a6364
hagevvashi <hagevvashi@gmail.com>
2020-10-11 17:25:41 +0900

    feat: add HHKB-C AC ans

--- atcoder/HHKB/C/src/main.c
+++ atcoder/HHKB/C/src/main.c
@@ -9,73 +9,20 @@
 #define min(p, q)((p) < (q) ? (p) : (q))
 #define swap(type, a, b) { type temp = a; a = b; b = temp; }
 
-
-int N;
-int * p;
-
-int c(const void * a, const void * b) {
-  return *(int *)a - *(int *)b;
-}
-
-int solve(int x) {
-  int * q;
-  q = array(x + 1, int);
-  rep(i, x + 1) {
-    q[i] = p[i];
-  }
-  // rep(i, x + 1) {
-  //   printf("q[%d]: %d\n", i, q[i]);
-  // }
-  qsort(q, x + 1, sizeof(int), c);
-  // rep(i, x + 1) {
-  //   printf("q[%d]: %d\n", i, q[i]);
-  // }
-  int r = 0;
-  rep(i, x + 1) {
-    if (i != 0) {
-      if (q[i] == q[i - 1]) {
-        continue;
-      }
-    }
-    // printf("r: %d\n", r);
-    // printf("i: %d\n", i);
-    // printf("q[%d]: %d\n", i, q[i]);
-    if (r == q[i]) {
-      r += 1;
-    } else {
-      break;
-    }
-  }
-  free(q);
-  return r;
-}
-
 int main() {
-
+  int N;
   scanf("%d", &N);
-  p = array(N, int);
 
-  /* rep(i, N) { */
-  /*   scanf("%d", p + i); */
-  /* } */
-
-  int * ans;
-  ans = array(N , int);
+  int acc[200010] = {0};
+  int ans = 0;
   rep(i, N) {
-    int r = 0;
-    int x;
-    scanf("%d", &x);
-    if (x != r) {
-      r += 1;
+    int p;
+    scanf("%d", &p);
+    acc[p] += 1;
+    while(acc[ans] != 0) {
+      ans += 1;
     }
-    ans[i] = r;
+    printf("%d\n", ans);
   }
-
-  rep(i, N) {
-    printf("%d\n", solve(i));
-  }
-
-  free(p);
-  free(ans);
   return 0;
 }
