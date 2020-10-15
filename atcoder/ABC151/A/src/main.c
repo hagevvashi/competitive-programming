63c97cfef8f319165530ddd18a908c2752212f33
hagevvashi <hagevvashi@gmail.com>
2020-10-15 09:45:39 +0900

    feat: add ABC151-A ac ans

---
+++ atcoder/ABC151/A/src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(type, a, b) { type temp = a; a = b; b = temp; }
+
+int main() {
+  char x;
+  scanf("%c",&x);
+  printf("%c\n", x + 1);
+  return 0;
+}
