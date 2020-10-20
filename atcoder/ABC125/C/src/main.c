d049986cd299973457c71051d213aa39145145a5
hagevvashi <hagevvashi@gmail.com>
2020-10-20 10:10:07 +0900

    wip: ABC125-C wip

--- atcoder/ABC125/C/src/main.c
+++ atcoder/ABC125/C/src/main.c
@@ -9,6 +9,30 @@
 #define min(p, q)((p) < (q) ? (p) : (q))
 #define swap(type, a, b) { type temp = a; a = b; b = temp; }
 
+int n;
+int *a;
+int *x;
+int gcd(int a,int b) {
+  if(a<b)swap(int,a,b);
+  int r=a%b;
+  while(r!=0){
+    a=b;
+    b=r;
+    r=a%b;
+  }
+  return b;
+}
 int main() {
+  scanf("%d",&n);
+  a=array(n,int);
+  x=array(n,int);
+  rep(i,n)scanf("%d",a+i);
+  x[0]=gcd(a[0],a[1]);
+  for(int i=1;i<n-1;i+=1){
+    x[i]=gcd(x[i-1],a[i+1]);
+  }
+  printf("%d\n",x[n-2]);
+  free(a);
+  free(x);
   return 0;
 }
