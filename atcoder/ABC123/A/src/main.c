93ff72d2517ec0e0621fd2323b51a23879c5abff
hagevvashi <hagevvashi@gmail.com>
2020-10-23 09:51:45 +0900

    feat: ABC123-A AC ans

---
+++ atcoder/ABC123/A/src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(type, a, b) { type temp = a; a = b; b = temp; }
+
+int main() {
+  int a,b,c,d,e,k;
+  scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&k);
+  if(k<e-a){
+    puts(":(");
+  }else{
+    puts("Yay!");
+  }
+  return 0;
+}
