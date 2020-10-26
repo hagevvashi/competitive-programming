77a220d2f019dc85a1b6ca3bc43f287ae47fb21a
hagevvashi <hagevvashi@gmail.com>
2020-10-26 09:40:50 +0900

    feat: add ARC106-A AC ans

--- atcoder/ARC106/A/src/main.c
+++ atcoder/ARC106/A/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+// #include <math.h>
 
 #define ll long long
 #define rep(i, n) for (ll i = 0; i < n; i = i + 1)
@@ -10,51 +11,63 @@
 #define swap(type, a, b) { type temp = a; a = b; b = temp; }
 
 ll n;
-ll t[200100]={0};
-ll f[200100]={0};
+ll t[30]={0};
+ll f[30]={0};
 ll aa=-1;
 ll ba=-1;
 
-ll pw(ll a,ll b){
-  ll x=1;
-  rep(i,b)x*=a;
-  return x;
+ll pw(ll x,ll y){
+  ll r=1;
+  rep(i,y)r*=x;
+  return r;
 }
 
-ll tf(){
-  ll i=0;
-  while(n>=pw(3,i)){
-    t[i]=pw(3,i);
-    i+=1;
-  }
-  return i;
-}
+/* ll tf(){ */
+/*   ll i=0; */
+/*   while(n>=pw(3,i)){ */
+/*     t[i]=pw(3,i); */
+/*     i+=1; */
+/*   } */
+/*   return i; */
+/* } */
 
-ll ff(){
-  ll i=0;
-  while(n>=pw(5,i)){
-    f[i]=pw(5,i);
-    i+=1;
-  }
-  return i;
-}
+/* ll ff(){ */
+/*   ll i=0; */
+/*   while(n>=pw(5,i)){ */
+/*     f[i]=pw(5,i); */
+/*     i+=1; */
+/*   } */
+/*   return i; */
+/* } */
+
+/* void solve(){ */
+/*   ll a=tf(); */
+/*   ll b=ff(); */
+
+/*   rep(i,a){ */
+/*     rep(j,b){ */
+/*       /\* printf("t[%d]: %lld\n",i,t[i]); *\/ */
+/*       /\* printf("f[%d]: %lld\n",j,f[j]); *\/ */
+/*       if(n==t[i]+f[j]){ */
+/*         aa=i; */
+/*         ba=j; */
+/*         return; */
+/*       } */
+/*     } */
+/*   } */
+/*   return; */
+/* } */
 
 void solve(){
-  ll a=tf();
-  ll b=ff();
-
-  rep(i,a){
-    rep(j,b){
-      /* printf("t[%d]: %lld\n",i,t[i]); */
-      /* printf("f[%d]: %lld\n",j,f[j]); */
-      if(n==t[i]+f[j]){
-        aa=i;
-        ba=j;
+  for(int a=1;a<38;a+=1){
+    for(int b=1;b<26;b+=1){
+      if(pw(3,a)+pw(5,b)==n){
+        aa=a;
+        ba=b;
         return;
       }
     }
   }
-  return;
 }
 
 int main() {
@@ -75,11 +88,16 @@ int main() {
   /* p/=5; */
   /* printf("b: %lld\n",b); */
   /* printf("p: %lld\n",p); */
+
+  /* ll foo=log(1000); */
+  /* printf("foo: %lld\n", foo); */
+
   solve();
   if(aa>-1 && ba>-1){
     printf("%lld %lld\n",aa,ba);
   }else{
     puts("-1");
   }
+
   return 0;
 }
