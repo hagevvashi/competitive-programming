9323582ee4050289b0d837ae69321dd779ebc42a
hagevvashi <hagevvashi@gmail.com>
2020-10-29 09:54:04 +0900

    feat: add ABC150-A ac ans

---
+++ atcoder/ABC150/A/src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(type, a, b) { type temp = a; a = b; b = temp; }
+
+int k,x;
+
+int main() {
+  scanf("%d%d",&k,&x);
+  if(500*k>=x)puts("Yes");
+  else puts("No");
+  return 0;
+}
