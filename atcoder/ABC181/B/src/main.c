e15db23438be5749566cd2f5a9f1f5707ca6c874
hagevvashi <hagevvashi@gmail.com>
2020-11-02 09:29:03 +0900

    feat: add ABC181-B ac ans

--- atcoder/ABC181/B/src/main.c
+++ atcoder/ABC181/B/src/main.c
@@ -9,6 +9,41 @@
 #define min(p, q)((p) < (q) ? (p) : (q))
 #define swap(type, a, b) { type temp = a; a = b; b = temp; }
 
+
+
+int solve1(int a,int b){
+  int ans=0;
+  for(int j=a;j<=b;j+=1){
+    ans+=j;
+  }
+  return ans;
+}
+
+ll solve2(int a,int b){
+  ll fpl=a+b;
+  // printf("fpl:%lld\n",fpl);
+  ll n=b-a+1;
+  // printf("n:%lld\n",n);
+  ll x=fpl*n;
+  // printf("x:%lld\n",x);
+  ll y=x/2;
+  // printf("y:%lld\n",y);
+  return y;
+}
+
 int main() {
+  int n;
+  scanf("%d",&n);
+  ll ans=0;
+  rep(i,n){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    // printf("ans:%lld\n",ans);
+    ll z=solve2(a,b);
+    // printf("z:%lld\n",z);
+    ans=ans+z;
+    // printf("ans:%lld\n",ans);
+  }
+  printf("%lld\n",ans);
   return 0;
 }
