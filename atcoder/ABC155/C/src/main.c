381e8f01f179654136d1ff2ab569deec51cef943
hagevvashi <hagevvashi@gmail.com>
2020-11-13 10:13:52 +0900

    wip: ABC155-C 途中

--- atcoder/ABC155/C/src/main.c
+++ atcoder/ABC155/C/src/main.c
@@ -11,14 +11,28 @@
 
 int n;
 char**s;
+int*cnt;
 
 int main() {
   scanf("%d",&n);
+  cnt=array(n,int);
   s=(char**)malloc(sizeof(char*)*n);
   rep(i,n){
     s[i]=(char*)malloc(sizeof(char)*11);
     scanf("%s",s[i]);
   }
+  puts("ここから入力状態の出力");
+  rep(i,n){
+    puts(s[i]);
+  }
+  rep(i,11){
+    for(int j=1;j<n;j+=1){
+      if(s[j-1][i] > s[j][i]){
+        swap(char*,s[j-1],s[j]);
+      }
+    }
+  }
+  puts("ここから入並び替え後の出力");
   rep(i,n){
     puts(s[i]);
   }
