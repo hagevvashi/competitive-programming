0cf2e57553cdf9aad74400dde40a3e56d78ae13c
hagevvashi <hagevvashi@gmail.com>
2020-11-14 20:53:26 +0900

    wip: ABC155-C 途中

--- atcoder/ABC155/C/src/main.c
+++ atcoder/ABC155/C/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define rep(i, n) for (int i = 0; i < n; i = i + 1)
 #define array(N, t) (t*)calloc(N, sizeof(t))
@@ -9,22 +10,26 @@
 #define min(p, q)((p) < (q) ? (p) : (q))
 #define swap(type, a, b) { type temp = a; a = b; b = temp; }
 
-int n;
-char**str_array;
-int*cnt;
-
-int cmpfunc(const void *a,const void *b){
-  return *(char*)a - *(char*)b;
+int cmpfunc(const void *str_a,const void *str_b){
+  // return -1;
+  return strcmp((char*)str_a,(char*)str_b);
 }
 
 int main() {
+  int n;
+  char**str_array;
+  int*cnt;
+
   scanf("%d",&n);
   cnt=array(n,int);
   str_array=(char**)malloc(sizeof(char*)*n);
+
   rep(i,n){
     str_array[i]=(char*)malloc(sizeof(char)*11);
     scanf("%s",str_array[i]);
   }
+
+  puts("");
 
   puts("ここから入力状態の出力");
   rep(i,n){
@@ -33,9 +38,10 @@ int main() {
 
   qsort(
     str_array,
-    sizeof(str_array) / sizeof(str_array[0]),
-    (size_t)sizeof(str_array[0]),
-    cmpfunc);
+    n,
+    (size_t)(sizeof(str_array[0])),
+    cmpfunc
+  );
 
   puts("ここから並び替え後の出力");
   rep(i,n){
@@ -47,6 +53,7 @@ int main() {
     free(str_array[i]);
   }
   free(str_array);
+  free(cnt);
 
   return 0;
 }
