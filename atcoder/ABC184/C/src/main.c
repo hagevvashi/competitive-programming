d2a25e7c7fe2bc4f019168398949fb5f27cc257f
hagevvashi <hagevvashi@gmail.com>
2020-11-23 01:17:04 +0900

    wip: ABC184-C 途中

--- atcoder/ABC184/C/src/main.c
+++ atcoder/ABC184/C/src/main.c
@@ -10,5 +10,33 @@
 #define swap(type, a, b) { type temp = a; a = b; b = temp; }
 
 int main() {
+  ll r1,c1,r2,c2;
+  scanf("%lld%lld",&r1,&c1);
+  scanf("%lld%lld",&r2,&c2);
+
+  if(r1+c1==r2+c2){
+    printf("%d\n",1);
+    return 0;
+  }
+  if(r1-c1==r2-c2){
+    printf("%d\n",1);
+    return 0;
+  }
+  if((labs(r1-r2)+labs(c1-c2))<=3){
+    printf("%d\n",1);
+    return 0;
+  }
+
+  if((labs(r1-r2)+labs(c1-c2))<=6){
+    printf("%d\n",2);
+    return 0;
+  }
+
+  ll nr2 = r2-r1;
+  ll nc2 = c2-c1;
+
+  if(nr2<0)nr2=-nr2;
+  if(nc2<0)nc2=-nc2;
+
   return 0;
 }
