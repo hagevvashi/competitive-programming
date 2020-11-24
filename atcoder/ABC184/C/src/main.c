4b870a6be5f1183bb6fe8483dd9d882fc4c38cf2
hagevvashi <hagevvashi@gmail.com>
2020-11-24 09:58:51 +0900

    wip: ABC184-C 理解中

--- atcoder/ABC184/C/src/main.c
+++ atcoder/ABC184/C/src/main.c
@@ -9,34 +9,22 @@
 #define min(p, q)((p) < (q) ? (p) : (q))
 #define swap(type, a, b) { type temp = a; a = b; b = temp; }
 
+int solve(int x,int y){
+  if (x == 0 && y == 0) return 0;
+  if (0 == x+y) return 1;
+  if (0 == x-y) return 1;
+  if (abs(x)+abs(y) <= 3) return 1;
+  if (0 == (x+y)%2) return 2;
+  if (abs(x)+abs(y) <= 6) return 2;
+  if (abs(x+y) <= 3) return 2;
+  if (abs(x-y) <= 3) return 2;
+  return 3;
+}
+
 int main() {
-  ll r1,c1,r2,c2;
-  scanf("%lld%lld",&r1,&c1);
-  scanf("%lld%lld",&r2,&c2);
-
-  if(r1+c1==r2+c2){
-    printf("%d\n",1);
-    return 0;
-  }
-  if(r1-c1==r2-c2){
-    printf("%d\n",1);
-    return 0;
-  }
-  if((labs(r1-r2)+labs(c1-c2))<=3){
-    printf("%d\n",1);
-    return 0;
-  }
-
-  if((labs(r1-r2)+labs(c1-c2))<=6){
-    printf("%d\n",2);
-    return 0;
-  }
-
-  ll nr2 = r2-r1;
-  ll nc2 = c2-c1;
-
-  if(nr2<0)nr2=-nr2;
-  if(nc2<0)nc2=-nc2;
-
+  ll x1,y1,x2,y2;
+  scanf("%lld%lld",&x1,&y1);
+  scanf("%lld%lld",&x2,&y2);
+  printf("%d\n",solve(x2-x1,y2-y1));
   return 0;
 }
