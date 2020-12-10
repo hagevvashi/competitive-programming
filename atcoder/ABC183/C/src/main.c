a8ff9f1c8c04b0d498510a438b7e5be0ae718966
hagevvashi <hagevvashi@gmail.com>
2020-12-10 10:59:59 +0900

    wip: refactor ABC183-C

--- atcoder/ABC183/C/src/main.c
+++ atcoder/ABC183/C/src/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ll long long
 #define rep(i, n) for (int i = 0; i < n; i = i + 1)
 #define array(N, t) (t*)calloc(N, sizeof(t))
-#define ll long long
 #define INF ((1LL << 62) - (1LL << 31))
 #define max(p, q)((p) > (q) ? (p) : (q))
 #define min(p, q)((p) < (q) ? (p) : (q))
