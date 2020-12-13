7bed081af0952bf48a9782fc63c979ffc8c3dd7b
hagevvashi <hagevvashi@gmail.com>
2020-12-13 23:35:14 +0900

    feat: add ABC185-B AC ans

--- atcoder/ABC185/B/src/main.c
+++ atcoder/ABC185/B/src/main.c
@@ -10,5 +10,47 @@
 #define swap(type, a, b) { type temp = a; a = b; b = temp; }
 
 int main() {
+  int n,m,t;
+  scanf("%d%d%d",&n,&m,&t);
+  int n_init=n;
+  int a[m],b[m];
+  rep(i,m) {
+    scanf("%d",a+i);
+    scanf("%d",b+i);
+  }
+  rep(i,m){
+    if(i==0){
+      n-=a[i];
+      if(n<=0){
+        break;
+      }
+      n+=b[i]-a[i];
+      if(n>n_init){
+        n=n_init;
+      }
+    }else{
+      n-=a[i]-b[i-1];
+      if(n<=0){
+        break;
+      }
+      n+=b[i]-a[i];
+      if(n>n_init){
+        n=n_init;
+      }
+    }
+    if(n<=0){
+      break;
+    }
+  }
+  if(n<=0){
+    puts("No");
+  }else{
+    n-=(t-b[m-1]);
+    if(n<=0){
+      puts("No");
+    } else {
+      puts("Yes");
+    }
+  }
   return 0;
 }
