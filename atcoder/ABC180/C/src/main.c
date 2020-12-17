0e5ad7f4ee07e450d3f605163a687ffe0875b528
hagevvashi <hagevvashi@gmail.com>
2020-12-17 23:40:25 +0900

    wip: ABC180-C 理解中

--- atcoder/ABC180/C/src/main.c
+++ atcoder/ABC180/C/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define ll long long
 #define INF ((1LL << 62) - (1LL << 31))
@@ -10,11 +11,26 @@
 #define swap(type, a, b) { type temp = a; a = b; b = temp; }
 
 int main() {
-  ll n;
-  scanf("%lld",&n);
-  rep(i,n,ll){
-    ll j=i+1;
-    if(n%j==0)printf("%lld\n",j);
+  ll N,i,x[1000000],n=0;
+
+  scanf("%lld",&N);
+
+  for(i=1;i<=sqrt(N);i++){
+    if(N%i==0){
+      printf("%lld\n",i);
+      x[n]=N/i;
+      n+=1;
+    }
   }
+  n-=1;
+
+  if(N/x[n]==x[n]){
+    n-=1;
+  }
+
+  for(i=n;i>=0;i-=1){
+    printf("%lld\n",x[i]);
+  }
+
   return 0;
 }
