2d3d1926ce21ad7e3425f3bdd38e63eb14c88c69
hagevvashi <hagevvashi@gmail.com>
2020-12-19 16:35:37 +0900

    feat: change template
    Because the case I need to specify type in "rep" macro increases recently, I modify "rep" macro in order to specify type.

--- template.c
+++ template.c
@@ -3,11 +3,11 @@
 
 #define ll long long
 #define INF ((1LL << 62) - (1LL << 31))
-#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define rep(i, n, type) for (type i = 0; i < n; i = i + 1)
 #define array(N, t) (t*)calloc(N, sizeof(t))
 #define max(p, q)((p) > (q) ? (p) : (q))
 #define min(p, q)((p) < (q) ? (p) : (q))
-#define swap(type, a, b) { type temp = a; a = b; b = temp; }
+#define swap(a, b, type) { type temp = a; a = b; b = temp; }
 
 int main() {
   return 0;
