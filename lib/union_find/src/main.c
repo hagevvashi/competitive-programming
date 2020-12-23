0fba318ec31df8a8f03622141b4f3a3140f924e8
hagevvashi <hagevvashi@gmail.com>
2020-12-23 14:52:28 +0900

    feat: add count function to union_find

--- lib/union_find/src/main.c
+++ lib/union_find/src/main.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define rep(i, n) for (int i = 0; i < n; i = i + 1)
-#define array(N, t) (t*)calloc(N, sizeof(t))
 #define ll long long
 #define INF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
 #define max(p, q)((p) > (q) ? (p) : (q))
 #define min(p, q)((p) < (q) ? (p) : (q))
-#define swap(type, a, b) { type temp = a; a = b; b = temp; }
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
 
-int N;
+int n;
 int * data;
 
 void init() {
   // child: parent's index
   // root: -size
-  data = array(N, int);
+  data = array(n, int);
 
-  rep(i, N) {
+  rep(i, n) {
     // at first, all data is root and size is -1
     data[i] = -1;
   }
@@ -43,7 +45,7 @@ int unite(int x, int y) {
   }
   // becasue data has minus size, reverse the inequality sign
   if (data[x] > data[y]) {
-    swap(int, x, y);
+    swap(x, y);
   }
   data[x] += data[y];
   data[y] = x;
@@ -54,14 +56,23 @@ int size(int x) {
   return -data[find(x)];
 }
 
+int count() {
+  int cnt = 0;
+  rep(i, n) {
+    if (find(i) == i) {
+      cnt += 1;
+    }
+  }
+  return cnt;
+}
+
 int main() {
-  scanf("%d", &N);
+  scanf("%d", &n);
 
   // initialize
   init();
 
-  int i= N - N;
-
+  int i= n - n;
   printf("size of %d is %d\n", i, size(i));
   printf("unite %d and %d\n", i, i + 1);
   unite(i, i + 1);
