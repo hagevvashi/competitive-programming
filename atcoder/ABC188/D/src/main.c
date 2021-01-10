c498af04cf402c3acfc8d5239984f410d2372d97
hagevvashi <hagevvashi@gmail.com>
2021-01-10 22:16:53 +0900

    wip: ABC188-D わからない

---
+++ atcoder/ABC188/D/src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
+
+int main(){
+  int n;
+  ll C;
+  scanf("%d%lld",&n,&C);
+  ll a[n],b[n],c[n];
+  rep(i,n){
+    scanf("%lld%lld%lld",a+i,b+i,c+i);
+  }
+  return 0;
+}
