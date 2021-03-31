29ae6bf7be116cfefeef9ea0a8c6653f4e6573a9
hagevvashi <hagevvashi@gmail.com>
2021-03-31 19:53:43 +0900

    wip: ABC197-C WA

--- atcoder/ABC197/C/src/main.c
+++ atcoder/ABC197/C/src/main.c
@@ -12,6 +12,50 @@
 #define swap(a, b) { int temp = a; a = b; b = temp; }
 #define lswap(a, b) { ll temp = a; a = b; b = temp; }
 
+ll yaru(int*shikiri,ll*a,int n){
+  // shikiri[0]=1 その他全部 0 のケース
+  ll katamari[10010]={0};
+  int katamari_i=0;
+  for(int i=0;i<n;i+=1){
+    katamari[katamari_i]|=a[i];
+    if(shikiri[i]==1){
+      katamari_i+=1;
+    }
+  }
+  ll re=katamari[0];
+  for(int i=1;i<=katamari_i;i+=1){
+    re^=katamari[i];
+  }
+  return re;
+}
+
 int main() {
+  int n;
+  scanf("%d",&n);
+  ll a[n];
+  rep(i,n){
+    scanf("%lld",a+i);
+  }
+  int shikiri[n-1];
+  rep(i,n-1){
+    shikiri[i]=0;
+  }
+
+  ll ans=LINF;
+  // 仕切りn-1個
+  // TODO: 何も仕切りがない状態は後で考える <- やらなくていい
+  rep(i,n-1){
+    // yaru??
+    shikiri[i]=1;
+    ans=min(ans,yaru(shikiri,a,n));
+    for(int j=i;j<n-1;j+=1){
+      shikiri[j]=1;
+      ans=min(ans,yaru(shikiri,a,n));
+    }
+    rep(k,n-1){
+      shikiri[k]=0;
+    }
+  }
+  printf("%lld\n",ans);
   return 0;
 }
