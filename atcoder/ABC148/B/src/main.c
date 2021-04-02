e9e7f7e975c7edcf470c85a239221335810de209
hagevvashi <hagevvashi@gmail.com>
2021-04-02 20:57:19 +0900

    feat: add ABC148-B AC ans

---
+++ atcoder/ABC148/B/src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF 2147483647
+#define LINF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
+
+char s[110],t[110];
+
+int main() {
+  int n;
+  scanf("%d",&n);
+  scanf("%s%s",s,t);
+  rep(i,n){
+    printf("%c%c",s[i],t[i]);
+  }
+  puts("");
+  return 0;
+}
