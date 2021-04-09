03c59ea54790a7965f32ead81d6ba1a8afbdb944
hagevvashi <hagevvashi@gmail.com>
2021-04-10 07:28:15 +0900

    feat: add ABC161-A ac ans

--- atcoder/ABC161/A/src/main.c
+++ atcoder/ABC161/A/src/main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF 2147483647
+#define LINF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
 
 int main() {
-  short input_array[3];
+  int x,y,z;
+  scanf("%d%d%d",&x,&y,&z);
 
-  scanf("%hd %hd %hd", input_array, input_array + 1, input_array + 2);
+  swap(x,y);
+  swap(x,z);
 
-  printf("%s", output);
+  printf("%d %d %d\n",x,y,z);
 
   return 0;
 }
