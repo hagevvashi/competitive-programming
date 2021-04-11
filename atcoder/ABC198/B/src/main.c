92975dbcc42c8d04c40659690ea0c91405d1cdc8
hagevvashi <hagevvashi@gmail.com>
2021-04-11 21:45:21 +0900

    wip: cannnot solve ABC198-B with C

---
+++ atcoder/ABC198/B/src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF 2147483647
+#define LINF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
+
+/**
+ * 回文 + n個の0 だったら頭に n個の0 をつければ大丈夫
+ */
+int main() {
+  ll n;
+  scanf("%lld",&n);
+  return 0;
+}
