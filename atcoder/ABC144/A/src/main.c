a7415aa2904970ab6cced6832a6fc584aa609521
hagevvashi <hagevvashi@gmail.com>
2021-04-29 00:14:17 +0900

    feat: add ABC144-A ac ans

---
+++ atcoder/ABC144/A/src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF 2147483647
+#define LINF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
+
+int main() {
+  int a,b;
+  scanf("%d%d",&a,&b);
+  if((a/10)>=1||(b/10)>=1){
+    puts("-1");
+  }else{
+    printf("%d\n",a*b);
+  }
+  return 0;
+}
