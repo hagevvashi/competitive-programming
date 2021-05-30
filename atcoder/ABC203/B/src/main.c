d71e3ba90750b552a7a003814483f5f943c5154b
hagevvashi <hagevvashi@gmail.com>
2021-05-30 21:42:49 +0900

    feat: add ABC203-B ac ans

---
+++ atcoder/ABC203/B/src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF 2147483647
+#define LINF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
+
+int main() {
+  int n,k;
+  scanf("%d%d",&n,&k);
+  int ans=0;
+  rep(i,n){
+    rep(j,k){
+      ans+=((i+1)*100)+(j+1);
+    }
+  }
+  printf("%d\n",ans);
+  return 0;
+}
