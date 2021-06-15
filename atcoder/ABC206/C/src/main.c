d2c3e36cf9b4f16880c9eadc7f1ee362a71b4678
hagevvashi <hagevvashi@gmail.com>
2021-06-16 07:19:15 +0900

    feat: add ABC206-C ac ans

--- atcoder/ABC206/C/src/main.c
+++ atcoder/ABC206/C/src/main.c
@@ -12,6 +12,50 @@
 #define swap(a, b) { int temp = a; a = b; b = temp; }
 #define lswap(a, b) { ll temp = a; a = b; b = temp; }
 
-int main() {
-  return 0;
+ll d[300010]={0};
+
+int compare_ll(const void *l,const void *r){
+  return *(ll*)l-*(ll*)r;
 }
+
+int main() {
+  ll n;
+  scanf("%lld",&n);
+  ll a[n];
+  lrep(i,n)scanf("%lld",a+i);
+  qsort(a, n, sizeof(ll), compare_ll);
+  /* rep(i,n){ */
+  /*   printf("%d ",a[i]); */
+  /* } */
+  /* puts(""); */
+  ll c=1;
+  lrep(i,n-1){
+    if(a[i]==a[i+1]){
+      c+=1;
+      if((i+1)==(n-1)){
+        d[c]+=1;
+      }
+    }
+    if(a[i]!=a[i+1]){
+      if(c!=1){
+        d[c]+=1;
+        c=1;
+      }
+    }
+  }
+  ll s=0;
+  lrep(i,300010){
+    if(d[i]!=0){
+      /* printf("i: %d\n",i); */
+      /* printf("d[i]: %d\n",d[i]); */
+      ll re=(i*(i-1))/2; // iC2
+      re*=d[i]; // iC2 を個数分
+      // printf("%lld\n",re);
+      s+=re;
+    }
+  }
+  ll ans=((n)*(n-1))/2;
+  ans-=s;
+  printf("%lld\n",ans);
+  return 0;
+};
