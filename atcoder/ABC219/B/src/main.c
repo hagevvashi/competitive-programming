1570a0a93c19b53bf45240cf36a147197c5e92df
hagevvashi <hagevvashi@gmail.com>
2021-09-18 21:38:29 +0900

    wip: add ABC219-B wa ans(C)

--- atcoder/ABC219/B/src/main.c
+++ atcoder/ABC219/B/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ll long long
 #define INF 2147483647
@@ -12,6 +13,22 @@
 #define swap(a, b) { int temp = a; a = b; b = temp; }
 #define lswap(a, b) { ll temp = a; a = b; b = temp; }
 
+char s[3][11];
+char t[4];
+
 int main() {
+  scanf("%s",s[0]);
+  scanf("%s",s[1]);
+  scanf("%s",s[2]);
+  scanf("%s",t);
+  int tlen=strlen(t);
+  // printf("tlen: %d\n",tlen);
+  rep(i,tlen){
+    // printf("i: %d\n", i);
+    int index=t[i]-'0';
+    index-=1;
+    printf("%s",s[index]);
+  }
+  puts("");
   return 0;
 }
