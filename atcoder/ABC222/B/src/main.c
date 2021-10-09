5b5c4bf8d049545e4301085976b07c4c3629b395
hagevvashi <hagevvashi@gmail.com>
2021-10-09 21:13:06 +0900

    feat: ABC222 B ac ans

---
+++ atcoder/ABC222/B/src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ll long long
+#define INF 2147483647
+#define LINF ((1LL << 62) - (1LL << 31))
+#define rep(i, n) for (int i = 0; i < n; i = i + 1)
+#define lrep(i, n) for (ll i = 0; i < n; i = i + 1)
+#define array(N, t) (t*)calloc(N, sizeof(t))
+#define max(p, q)((p) > (q) ? (p) : (q))
+#define min(p, q)((p) < (q) ? (p) : (q))
+#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define lswap(a, b) { ll temp = a; a = b; b = temp; }
+
+int main() {
+  int n,p;
+  scanf("%d%d",&n,&p);
+  int a[n];
+  rep(i,n)scanf("%d",a+i);
+  int ans=0;
+  rep(i,n){
+    if(a[i]<p){
+      ans+=1;
+    }
+  }
+  printf("%d\n",ans);
+  return 0;
+}
